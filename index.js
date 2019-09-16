7c04c4d8f5016a558ef44769bce52036d4ebf3f9
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-16 10:45:15 -0700

    Adding middleware

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -74,6 +74,28 @@ server.get("/hash", (req, res) => {
   const hash2 = bcrypt.hashSync(name, 12);
   res.send(`the hash for ${name} is ${hash2}`);
 })
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if(username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+          next()
+        } else {
+          res.status(401).json({message: "Invalid Creds"})
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unexpected error' });
+      })
+  } else {
+    res.status(400).json({ message: "No creds provided." })
+  }
+}
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
